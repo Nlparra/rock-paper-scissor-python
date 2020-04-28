76cc16a1104d4e963200c49709fc9e21cf547dcf
Nelson Parra <nlparra.dev@gmail.com>
2020-04-28 18:06:23 -0400

    working on logic code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,7 +10,7 @@ def pc_choice():
     random_hand = hand[random.randint(0, len(hand))]
     return (random_hand)
 
-def game():
+def game(user_hand):
         if user_hand == pc_choice():
             print("Tied")
         elif user_hand == "rock":
@@ -18,18 +18,18 @@ def game():
                 print("You lose!", pc_choice(), "covers", user_hand)
             else:
                 print("You win!", user_hand, "smashes", pc_choice())
-        elif user_hand == "Paper":
-            if pc_choice() == "Scissors":
-                print("You lose!", pc_choice(), "cut", user_hand)
-            else:
-                print("You win!", user_hand, "covers", pc_choice())
-        elif user_hand == "Scissors":
-            if pc_choice() == "Rock":
-                print("You lose...", pc_choice(), "smashes", user_hand)
-            else:
-                print("You win!", user_hand, "cut", pc_choice())
-        else:
-            print("That's not a valid play. Check your spelling!")
+        # elif user_hand == "Paper":
+        #     if pc_choice() == "Scissors":
+        #         print("You lose!", pc_choice(), "cut", user_hand)
+        #     else:
+        #         print("You win!", user_hand, "covers", pc_choice())
+        # elif user_hand == "Scissors":
+        #     if pc_choice() == "Rock":
+        #         print("You lose...", pc_choice(), "smashes", user_hand)
+        #     else:
+        #         print("You win!", user_hand, "cut", pc_choice())
+        # else:
+        #     print("That's not a valid play. Check your spelling!")
 
 print(game())
 
